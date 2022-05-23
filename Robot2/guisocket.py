c62b0f600e947b6fee54be0ff021b705b800614d
Md. Al-Masrur Khan <33350185+Masrur02@users.noreply.github.com>
2022-05-23 11:02:53 +0900

    Update guisocket.py

--- Robot2/guisocket.py
+++ Robot2/guisocket.py
@@ -9,13 +9,15 @@ import os
 from tkinter import *
 
 class Socket:
-    host='192.168.0.3'
+    host='192.168.0.20'
     # host = '127.0.0.1'
     def __init__(self):
         self.on_message = {
             'frame': self.onFrameAsync,
+            'bw_frame':self.onbwFrameAsync,
             'front_frame':self.onFrontFrameAsync,
             'signalData':self.onsignalData,
+            "feedData":self.onfeedData,
             'u_there': lambda x: None
 
         }
@@ -39,23 +41,56 @@ class Socket:
 
     def onFrameAsync(self, frame):
         Thread(target=self.onFrame, args=(frame,)).start()
+
+    def onbwFrameAsync(self, bw_frame):
+        Thread(target=self.onbwFrame, args=(bw_frame,)).start()
+
+
     
     def onFrontFrameAsync(self, front_frame):
         Thread(target=self.onFrontFrame, args=(front_frame,)).start()
     
     def onsignalData(self, signalData):
         Thread(target=self.onSignal, args=(signalData,)).start()
+    def onfeedData(self, feedData):
+        Thread(target=self.onfeed, args=(feedData,)).start()
+    
 
     def onSignal(self,signalData):
-
+        self.gui.plot.clear()
+        self.gui.plt.clear()
         self.x = signalData.get("X")
         self.y = signalData.get("y")
         self.y1 = signalData.get("y1")
+        #print(self.y)
+        #print(self.y1)
         
         self.gui.plot.plot(self.x, self.y,color='red')
         self.gui.plt.plot(self.x, self.y1,color='blue')
         self.gui.canvas.draw()
+        self.gui.plot.clear()
+        self.gui.plt.clear()
         self.gui.Start_button.config(state=NORMAL)
+    def onfeed(self,feedData):
+        print(feedData)
+        self.gui.Start_button.config(state=NORMAL)
+        self.gui.Send_button.config(state=NORMAL)
+        self.gui.Excel_button.config(state=NORMAL)
+        self.gui.Right_button.config(state=NORMAL)
+        self.gui.Forward_button.config(state=NORMAL)
+        self.gui.Left_button.config(state=NORMAL)
+        self.gui.Back_button.config(state=NORMAL)
+        self.gui.A_button.config(state=NORMAL)
+        self.gui.speed_Send_button.config(state=NORMAL)
+        self.gui.position_Zero_button.config(state=NORMAL)
+        
+        self.gui.Original_button.config(state=NORMAL)
+        self.gui.Off_button.config(state=NORMAL)
+        print("All Done")
+        
+        
+        
+        
 
     def save(self):
         import pandas as pd
@@ -94,7 +129,8 @@ class Socket:
 
         
     
-        frame = cv2.flip(front_frame, 1)
+        frame = cv2.flip(front_frame, -1)
+        frame = cv2.flip(frame, -1)
         cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
         img = PIL.Image.fromarray(cv2image)
         img = img.resize((600, 430))
@@ -120,7 +156,9 @@ class Socket:
         #flag = True
 
         
-        frame = cv2.flip(frame, 1)
+        frame = cv2.flip(frame, -1)
+        frame = cv2.flip(frame, -1)
+        frame2 = cv2.resize(frame, (512,512))
         cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
         img = PIL.Image.fromarray(cv2image)
         img = img.resize((300, 250))
@@ -147,23 +185,64 @@ class Socket:
         time = time.time()
         file = str(time)
        
-        cv2.imwrite(folder1 +'/' + str(file) + '.jpg', frame)
+        cv2.imwrite(folder1 +'/' + str(file) + '.jpg', frame2)
         
         
         
         
 
         self.gui.top.update()
-        
 
+    def onbwFrame(self, bw_frame):
+        # global flag
+        # flag = True
+
+        bw_frame = cv2.flip(bw_frame, 1)
+        bw_frame = cv2.flip(bw_frame, 1)
+        bw_frame2 = cv2.resize(bw_frame, (512,512))
+        cv2image = cv2.cvtColor(bw_frame, cv2.COLOR_BGR2RGBA)
+        #img = PIL.Image.fromarray(cv2image)
+        img = cv2image.resize((300, 250))
+
+        #imgtk = ImageTk.PhotoImage(image=img)
+        # label = Label(self.gui.Video2, image=imgtk)
+        # label.place(x=10,y=50)
+        #self.gui.lmain.imgtk = imgtk
+
+        #self.gui.lmain.configure(image=imgtk)
+        #self.gui.lmain.place(x=5, y=15)
+        import pandas as pd
+        from datetime import datetime
+        import time
+        path1 = "BW"
+
+        today = datetime.now()
+
+        directory1 = today.strftime('%Y%m%d')
+
+        folder1 = os.path.join(path1, directory1)
+        if not os.path.exists(folder1):
+            os.makedirs(folder1)
+        time = time.time()
+        file = str(time)
+
+        cv2.imwrite(folder1 + '/' + str(file) + '.jpg', bw_frame2)
+
+        self.gui.top.update()
     def video(self):
         self.gui.Original_button.config(state=DISABLED)
         self.__protocol.send_message('command', 'start_video')
+        self.__protocol.send_message('command', 'start_bw_video')
+
         
     
     def front_video(self):
         self.gui.front_button.config(state=DISABLED)
         self.__protocol.send_message('command', 'start_front_video')
+        
+    def zero(self):
+        
+        self.__protocol.send_message('command', 'zero')
     
     
     def F_on_press(self,event):
@@ -190,6 +269,24 @@ class Socket:
         self.__protocol.send_message("command", "startSignal")
 
         self.gui.Start_button.config(state=DISABLED)
+    
+    def semiAuto(self):
+        self.gui.Start_button.config(state=DISABLED)
+        self.gui.Send_button.config(state=DISABLED)
+        self.gui.Excel_button.config(state=DISABLED)
+        self.gui.Right_button.config(state=DISABLED)
+        self.gui.Forward_button.config(state=DISABLED)
+        self.gui.Left_button.config(state=DISABLED)
+        self.gui.Back_button.config(state=DISABLED)
+        self.gui.A_button.config(state=DISABLED)
+        self.gui.speed_Send_button.config(state=DISABLED)
+        self.gui.position_Zero_button.config(state=DISABLED)
+        
+        self.gui.Original_button.config(state=DISABLED)
+        self.gui.Off_button.config(state=DISABLED)
+        self.__protocol.send_message("command", "semiAuto")
+
+        #self.gui.Start_button.config(state=DISABLED)
 
     def trigger (self):
         time = self.gui.time_entry.get()
@@ -198,6 +295,15 @@ class Socket:
 
         triggerData = {"time": time, "trigger": trigger}
         self.__protocol.send_message('triggerData', triggerData)
+
+    def grid (self):
+        G_X = self.gui.GX_entry.get()
+        G_Y = self.gui.GY_entry.get()
+
+
+
+        gridData = {"G_X": G_X, "G_Y": G_Y}
+        self.__protocol.send_message('gridData', gridData)
 
     def speed (self):
         speed=self.gui.speed_entry.get()
