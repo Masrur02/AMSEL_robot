3786b3830b8ab996cb782f701886cb1da1f99722
Md. Al-Masrur Khan <33350185+Masrur02@users.noreply.github.com>
2022-11-25 17:56:36 +0900

    Update main.py

--- Robot2/main.py
+++ Robot2/main.py
@@ -1,11 +1,10 @@
+
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jun 18 23:39:25 2021
 
 @author: almas
 """
-
-
 
 from guiwin import Gui
 from guisocket import Socket
@@ -15,9 +14,4 @@ if __name__ == '__main__':
     gui = Gui(socket)
     socket.set_gui(gui)
     gui.top.mainloop()
-
-
-
-
-
-
+    
